1cdc0373830bf8af6fff8af79bcf03062d666fe1
inoueyouhei <inoueyouhei@localhost>
2022-02-17 23:07:59 +0900

    EncodingFilter作成、定数ファイル作成

---
+++ src/main/java/constants/ForwardConst.java
@@ -0,0 +1,62 @@
+package constants;
+
+/*
+ * リクエストパラメータの変数名、変数値、jspファイルの名前等画面遷移に関わる値を定義するEnumクラス
+ * */
+public enum ForwardConst {
+
+    // action
+    ACT("action"),
+    ACT_TOP("Top"),
+    ACT_EMP("Enployee"),
+    ACT_REP("Report"),
+    ACT_AUTH("Auth"),
+
+    // command
+    CMD("command"),
+    CMD_NONE("NONE"),
+    CMD_INDEX("index"),
+    CMD_SHOW("show"),
+    CMD_SHOW_LOGIN("showLogin"),
+    CMD_LOGIN("login"),
+    CMD_LOGOUT("logout"),
+    CMD_NEW("entryNew"),
+    CMD_CREATE("create"),
+    CMD_EDIT("edit"),
+    CMD_UPDATE("update"),
+    CMD_DESTROY("destroy"),
+
+    // jsp
+    FW_ERR_UNKNOWN("error/unknown"),
+    FW_TOP_INDEX("topPage/index"),
+    FW_LOGIN("login/login"),
+    FW_EMP_INDEX("employee/index"),
+    FW_EMP_SHOW("employee/show"),
+    FW_EMP_NEW("employee/new"),
+    FW_EMP_EDIT("employee/edit"),
+    FW_REP_INDEX("reports/index"),
+    FW_REP_SHOW("reports/show"),
+    FW_REP_NEW("reports/new"),
+    FW_REP_EDIT("reports/edit");
+
+
+    /*
+     * 文字列
+     * */
+    private final String text;
+
+    /*
+     * コンストラクタ
+     * */
+    private ForwardConst(final String text) {
+        this.text = text;
+    }
+
+    /*
+     * 値（文字列）取得
+     * */
+    public String getValue() {
+        return this.text;
+    }
+
+}
