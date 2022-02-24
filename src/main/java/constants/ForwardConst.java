e4c1fc35432f19a49f07b13b5002782d2a87bd1f
inoueyouhei <inoueyouhei@localhost>
2022-02-24 22:25:08 +0900

    従業員newアクションの作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -8,7 +8,7 @@ public enum ForwardConst {
     // action
     ACT("action"),
     ACT_TOP("Top"),
-    ACT_EMP("Enployee"),
+    ACT_EMP("Employee"),
     ACT_REP("Report"),
     ACT_AUTH("Auth"),
 
